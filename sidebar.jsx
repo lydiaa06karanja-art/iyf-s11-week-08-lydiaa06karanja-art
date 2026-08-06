c0a10012a6578bca572a5268111bb389319a3e59
Lydia karanja <lydiaa06karanja@gmail.com>
2026-08-06 22:12:47 +0300

    Create sidebar.jsx

---
+++ sidebar.jsx
@@ -0,0 +1,14 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h3>Categories</h3>
+      <ul>
+        <li>General</li>
+        <li>Announcements</li>
+        <li>Help</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
