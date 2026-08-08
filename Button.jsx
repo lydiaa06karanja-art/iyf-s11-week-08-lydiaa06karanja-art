3c9761fab9b3c74b0d81bcafae5f1bc471476e9b
Lydia karanja <lydiaa06karanja@gmail.com>
2026-08-08 21:35:36 +0300

    Add default props to Button component

--- Button.jsx
+++ Button.jsx
@@ -1,6 +1,10 @@
-function Button({ text, onClick }) {
+
+function Button({ text = "Click me", variant = "primary", onClick }) {
   return (
-    <button className="btn" onClick={onClick}>
+    <button 
+      className={`btn btn-${variant}`} 
+      onClick={onClick}
+    >
       {text}
     </button>
   );
